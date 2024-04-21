4c9e48a49cc5fe8e56b85d6ad8bfad9dc3c86dce
Ch4vs <marcochaves0229@gmail.com>
2024-04-20 18:00:56 -0600

    update

--- src/main/resources/templates/scriptConsultarTematica.js
+++ src/main/resources/templates/scriptConsultarTematica.js
@@ -1,10 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const opciones = document.getElementById('opciones');
-    const correo = localStorage.getItem('correoo'); // Obtener el correo de localStorage
 
-    const url = `http://localhost:9090/api/v1/Tematica/lista?correo=${correo}`;
-
-    fetch(url)
+    fetch('http://localhost:9090/api/v1/Tematica/lista')
         .then(response => response.json())
         .then(data => {
             data.forEach(tematica => {
@@ -15,37 +12,128 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         })
         .catch(error => console.error('Error:', error));
-
-
-    function obtenerTextoSeleccionado() {
-        const opcionesTexto = document.getElementById("opcionesTexto");
-        const textoSeleccionado = opcionesTexto.options[opcionesTexto.selectedIndex].text;
-        return textoSeleccionado;
-    }
-
-    async function consultarChatGPT() {
-        console.log('Consultar ChatGPT clicado');
-        const textoSelect = obtenerTextoSeleccionado();
-        const url = 'http://localhost:9090/api/v1/Tematica/consultaChatGPT';
-        console.log('URL de la solicitud:', url);
-        const datos = {
-            texto: textoSelect
-        };
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(datos)
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            });
-    }
-
-    function mostrarResultado(resultado) {
-        const ventanaEmergente = window.open('', '_blank', 'width=600,height=400');
-        ventanaEmergente.document.write(`<html><head><title>Resultado</title></head><body><h1>Palabras Clave Generadas:</h1><p>${resultado}</p></body></html>`);
-    }
 });
+
+function obtenerTextoSeleccionado() {
+    var opcionesTexto = document.getElementById("opcionesTexto");
+    var textoSeleccionado = opcionesTexto.options[opcionesTexto.selectedIndex].text;
+    return textoSeleccionado;
+}
+
+async function consultarChatGPT() {
+    var palabrasClave = localStorage.getItem('palabrasClave');
+    console.log('Consultar ChatGPT clicado');
+    var textoSelect= palabrasClave;
+    var url = 'http://localhost:9090/api/v1/Tematica/consultaChatGPT';
+    console.log('URL de la solicitud:', url);
+    var datos = {
+        texto: textoSelect
+    }
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
+    })
+        .then(response=>response.json())
+        .then(data=>{
+            console.log(data);
+        })
+
+}
+
+async function consultarSentimiento() {
+    console.log('Consultar ChatGPT clicado');
+    var textoSelect= obtenerTextoSeleccionado();
+    var url = 'http://localhost:9090/api/v1/Tematica/consultaSentimiento';
+    console.log('URL de la solicitud:', url);
+    var datos = {
+        texto: textoSelect
+    }
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
+    })
+        .then(response=>response.json())
+        .then(data=>{
+            console.log(data);
+        })
+
+}
+
+async function generarPalabrasClave() {
+    console.log('Consultar ChatGPT clicado');
+    var textoSelect= obtenerTextoSeleccionado();
+    var url = 'http://localhost:9090/api/v1/Tematica/consultaPalabrasClave';
+    console.log('URL de la solicitud:', url);
+    var datos = {
+        texto: textoSelect
+    }
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
+    })
+        .then(response=>response.json())
+        .then(data=>{
+            console.log(data);
+        })
+
+}
+
+
+
+async function mostrarRespuestaChatGPT() {
+    try {
+        const response = await fetch('respuestaChatGPT.txt');
+        if (response.ok) {
+            const contenido = await response.text();
+            alert(contenido);
+        } else {
+            console.log('El archivo respuesta.txt no existe.');
+        }
+    } catch (error) {
+        console.error('Error al cargar el contenido de respuesta.txt:', error);
+    }
+}
+
+async function mostrarRespuestaSentimiento() {
+    try {
+        const response = await fetch('respuestaSentimiento.txt');
+        if (response.ok) {
+            const contenido = await response.text();
+            alert(contenido);
+        } else {
+            console.log('El archivo respuesta.txt no existe.');
+        }
+    } catch (error) {
+        console.error('Error al cargar el contenido de respuesta.txt:', error);
+    }
+}
+
+async function mostrarRespuestaPalabrasClave() {
+    try {
+        const response = await fetch('respuestaPalabrasClave.txt');
+        if (response.ok) {
+            const contenido = await response.text();
+            localStorage.setItem('palabrasClave', contenido);
+            alert(contenido);
+        } else {
+            console.log('El archivo respuesta.txt no existe.');
+        }
+    } catch (error) {
+        console.error('Error al cargar el contenido de respuesta.txt:', error);
+    }
+}
+
+
+function mostrarResultado(resultado) {
+    const ventanaEmergente = window.open('', '_blank', 'width=600,height=400');
+    ventanaEmergente.document.write(`<html><head><title>Resultado</title></head><body><h1>Palabras Clave Generadas:</h1><p>${resultado}</p></body></html>`);
+}
