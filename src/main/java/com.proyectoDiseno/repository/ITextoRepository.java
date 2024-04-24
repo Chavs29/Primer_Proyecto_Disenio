317543a40b5fa243e63e28110ca135a551c5a0e4
JimenaCarvajal <118943312+JimenaCarvajal@users.noreply.github.com>
2024-04-24 15:37:00 -0600

    Update ITextoRepository.java

--- src/main/java/com.proyectoDiseno/repository/ITextoRepository.java
+++ src/main/java/com.proyectoDiseno/repository/ITextoRepository.java
@@ -5,8 +5,8 @@ import com.proyectoDiseno.model.Texto;
 import java.util.List;
 
 public interface ITextoRepository {
-    List<Texto> findAll(); // Obtener todos los textos
+    List<Texto> findAll();
+    List<Texto> findByTematica(String nombre);
     List<Texto> findTexto(String texto);
-    List<Texto> findByTematica(String nombre); // Obtener textos por tematica
     void save(Texto texto, String nombre_tematica);
 }
