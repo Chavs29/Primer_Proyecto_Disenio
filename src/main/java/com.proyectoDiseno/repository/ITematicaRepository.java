a06394437e9b891fccf18fe27e4155aea55fd059
JimenaCarvajal <118943312+JimenaCarvajal@users.noreply.github.com>
2024-04-24 15:36:50 -0600

    Update ITematicaRepository.java

--- src/main/java/com.proyectoDiseno/repository/ITematicaRepository.java
+++ src/main/java/com.proyectoDiseno/repository/ITematicaRepository.java
@@ -4,6 +4,6 @@ import com.proyectoDiseno.model.Tematica;
 import java.util.List; // Importa la clase List
 
 public interface ITematicaRepository {
-    List<Tematica> findAll(String correo); // Define el método findAll para obtener todas las temáticas
+    List<Tematica> findAll(String correo);
     void save(Tematica tematica, String correo);
 }
